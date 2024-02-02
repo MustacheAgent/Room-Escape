a92b03c46384af1edf90e750bc701e863a7159d1
MustacheAgent <dimonus98@yandex.ru>
2024-02-02 21:34:53 +0300

    оформлена ванная

--- Assets/Scripts/Dissolve.cs
+++ Assets/Scripts/Dissolve.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 
@@ -5,12 +6,16 @@ public class Dissolve : MonoBehaviour
 {
     [SerializeField] private float duration = 1f;
     
-    private Material _mat;
+    private Material[] _mat = Array.Empty<Material>();
     private static readonly int AlphaClip = Shader.PropertyToID("_AlphaClip");
 
     public void SetVisible(bool hide)
     {
-        if (!_mat) _mat = GetComponent<Renderer>().material;
-        _mat.DOFloat(hide ? 1.1f : 0, AlphaClip, duration).SetEase(Ease.Linear);
+        if (_mat.Length == 0) _mat = GetComponent<Renderer>().materials;
+
+        foreach (var material in _mat)
+        {
+            material.DOFloat(hide ? 1.1f : 0, AlphaClip, duration).SetEase(Ease.Linear);
+        }
     }
 }
